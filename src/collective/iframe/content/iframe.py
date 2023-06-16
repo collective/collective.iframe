d1983539c2184d6cf7bad836ef64004d68e337ae
Maurits van Rees <maurits@vanrees.org>
2023-06-16 19:37:46 +0200

    Add fields.

--- src/collective/iframe/content/iframe.py
+++ src/collective/iframe/content/iframe.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
+from plone.app.textfield import RichText
 # from plone.autoform import directives
 from plone.dexterity.content import Item
 
@@ -8,55 +7,63 @@ from plone.supermodel import model
 
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-# from collective.iframe import _
+from collective.iframe import _
 
 
 class IIframe(model.Schema):
-    """Marker interface and Dexterity Python Schema for Iframe"""
+    """Marker interface and Dexterity Python Schema for Iframe
 
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
+    For inspiration for more fields:
+    https://github.com/collective/Products.windowZ/blob/master/Products/windowZ/content/Window.py
+    https://github.com/collective/collective.tiles.iframembed/blob/master/src/collective/tiles/iframembed/interfaces.py
 
-    # model.load('iframe.xml')
+    Products.windowZ has:
+    hide_metadata
+    use_base_url
+    catalog_page_content
+    show_reference
+    inherit_protocol
+    """
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    remote_url = schema.URI(
+        title=_("label_url_to_embed", "Url to embed"), required=False
+    )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    page_width = schema.TextLine(
+        title=_("Page width"),
+        # TODO: text taken over from Products.windowZ
+        # Not sure if we want to have a control panel.
+        description=_(
+            "help_page_width",
+            default=(
+                "Enter a value for the page width. If it is not provided, "
+                "the page width will assume the default value defined in "
+                "the site setup. You may use %, px, em, etc."
+            ),
+        ),
+        required=False,
+    )
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    page_height = schema.TextLine(
+        title=_("Page height"),
+        description=_(
+            "help_page_height",
+            default=(
+                "Enter a value for the page height. If it is not provided, "
+                "the page height will assume the default value defined in "
+                "the site setup. You may use %, px, em, etc."
+            ),
+        ),
+        required=False,
+    )
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
+    text_above = RichText(title=_("Text above"), required=False)
 
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    text_below = RichText(title=_("Text below"), required=False)
 
 
 @implementer(IIframe)
